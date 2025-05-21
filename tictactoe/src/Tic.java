7ef3f7a732e7feadee965a55f096a68ae263c6e8
Jorge <jorgecarrillo6571@gmail.com>
2025-05-21 13:02:44 -0600

    created the buttons grid

--- tictactoe/src/Tic.java
+++ tictactoe/src/Tic.java
@@ -6,6 +6,7 @@ import java.awt.event.*;
 
 public class Tic implements ActionListener{
 	private JFrame frame;
+	private JPanel panel; 
 
 
 	public Tic (){
@@ -13,8 +14,17 @@ public class Tic implements ActionListener{
 		frame = new JFrame ("TicTacToe");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 		frame.setSize(420,550);
-		frame.setLayout(null);
-		frame.setVisible(true); 
+//		frame.setLayout(null); esto no es necesario y causa errror con gridlayout 
+		
+                panel = new JPanel(new GridLayout(3,3,10,10));
+		for (int i =0; i <9; i++){
+			JButton button = new JButton ("H"); 
+			panel.add(button); 
+		}
+	 
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);	
 
 	}	
 
